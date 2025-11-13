b9eb9c0ead533c9a65f6d8a5fb202646b382bbcf
Roman Kniazhyk <romanknyazhyk@gmail.com>
2025-11-13 11:26:20 +0200

    Add warn

--- components/OnboardingAvatar/OnboardingAvatar.tsx
+++ components/OnboardingAvatar/OnboardingAvatar.tsx
@@ -19,6 +19,7 @@ export default function ProfileAvatar() {
 
   const handleButtonClick = () => {
     fileInputRef.current?.click();
+    console.warn(error);
   };
 
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
